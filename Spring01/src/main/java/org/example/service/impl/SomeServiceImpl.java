a5bedae902fa94c137299ff1fc1347b757bbe5d6
PasserByzh <544740149@qq.com>
2022-05-25 09:29:11 +0800

    update

--- Spring01/src/main/java/org/example/service/impl/SomeServiceImpl.java
+++ Spring01/src/main/java/org/example/service/impl/SomeServiceImpl.java
@@ -3,6 +3,11 @@ package org.example.service.impl;
 import org.example.service.SomeService;
 
 public class SomeServiceImpl implements SomeService {
+
+    public SomeServiceImpl() {
+        System.out.println("执行SomeServiceImpl 构造方法");
+    }
+
     @Override
     public void doSome() {
         System.out.println("执行了SomeServiceImpl 的 doSome()方法！");
--- Spring01/src/test/java/org/example/AppTest.java
+++ Spring01/src/test/java/org/example/AppTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertTrue;
 import org.example.service.SomeService;
 import org.example.service.impl.SomeServiceImpl;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Unit test for simple App.
@@ -22,8 +24,25 @@ public class AppTest
 
     @Test
     public void myTest(){
-        SomeService service = new SomeServiceImpl();
-        service.doSome();
+//        SomeService service = new SomeServiceImpl();
+//        service.doSome();
+
+        /*
+        * Spring 默认创建对象的时间：在创建Spring容器的时候，会创建配置文件中的所有对象。
+        * */
+        // 1. 指定Spring配置文件的名称
+        String config = "beans.xml";
+        // 2. 创建表示Spring容器的对象，ApplicationContext
+        // ApplicationContext 表示Spring容器，通过容器就能获取对象
+        // ClassPathXmlApplicationContext：表示从类路径中加载Spring的配置文件
+        ApplicationContext context = new ClassPathXmlApplicationContext(config);
+
+        // 从容器中获取某个对象，调用对象的方法
+        SomeService someService = (SomeService) context.getBean("SomeService");
+        // 使用Spring创建好的对象
+        someService.doSome();
+
+
     }
 
 }
